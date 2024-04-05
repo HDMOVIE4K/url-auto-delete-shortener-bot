0df883a61d9bf38e1b6ed730f563c1b1a17ef1cb
HDMOVIE4K <166133586+HDMOVIE4K@users.noreply.github.com>
2024-04-05 21:10:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '20404227'))
+API_HASH = environ.get('API_HASH', '7fa6d7f29d008a45e018a16e575ef7ad')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6653210518:AAGshd0YGFEvWbUMUgtAU1pwBd58LV9kX8c")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
