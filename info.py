8c75c5bbbae9fec243d84a0310e468004f2d3c05
HDMOVIE4K <166133586+HDMOVIE4K@users.noreply.github.com>
2024-04-05 22:30:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
       # URL Shortener #
 
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'linkshortify.com')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '8edd60727fa7dc7d81b0444b0c5ea215c3c08481')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
